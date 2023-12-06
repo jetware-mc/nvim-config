821afb65b7987b7a57788db9b488282cd8589d08
thedevjade <jadeisstolen@gmail.com>
2023-12-06 11:45:43 -0800

    Added all configuration and themes

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,27 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("todo-comments").setup {
+
+      }
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      -- configuration options...
+    },
+  },
+
+  {
+    "sainnhe/sonokai",
+    init = function() -- init function runs before the plugin is loaded
+      vim.g.sonokai_style = "espresso"
+    end,
+  },
 }
